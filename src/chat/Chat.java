93183baf702eb1e101583e91a7f17a95ea2bcd9c
Valencia <starrv2011@gmail.com>
2021-03-11 16:14:05 -0500

    changes

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -1,13 +1,5 @@
 package chat;
 
-import static javax.measure.unit.SI.KILOGRAM;
-
-import java.util.Map;
-
-import javax.measure.quantity.Mass;
-import org.jscience.physics.model.RelativisticModel;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.jscience.physics.amount.Amount;
 
 public class Chat
 {
@@ -18,11 +10,4 @@ public class Chat
 		app.setVisible(true);
 	}
 	
-	@RequestMapping("/hello")
-	String hello(Map<String, Object> model) {
-	    RelativisticModel.select();
-	    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-	    model.put("science", "E=mc^2: 12 GeV = " + m.toString());
-	    return "hello";
-	}
 }
